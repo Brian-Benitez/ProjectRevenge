7726e354ba37778091eb63f6aaefa790d68123b6
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2025-12-10 18:54:03 -0500

    Updated: Falling damage object

--- 2D_TurnBased_Template/Assets/Code/EnviromentCode/MovingObject.cs
+++ 2D_TurnBased_Template/Assets/Code/EnviromentCode/MovingObject.cs
@@ -3,49 +3,54 @@ using UnityEngine;
 
 public class MovingObject : MonoBehaviour
 {
+    [Header("Speeds")]
     public float DelayRestart;
-    public float MovementSpeed;
+    public float RetractSpeed;
+    public float FallingSpeed;
+    public float MaxWaitTime;
 
-    public float MaxFallDistance;
-    public float MinFallDistance;   
-
+    [Header("GameObjects")]
     public GameObject StartPoint;
     public GameObject EndPoint;
 
+    [Header("Bools")]
     public bool GoBack;
     public bool GoForward = true;
+    float _waitTime;
 
     private void Update()
     {
-        if(Vector2.Distance(transform.position, StartPoint.transform.position) >= MaxFallDistance)
-        {
-            GoBack = true;
-            GoForward = false;
-        }
-
-        if (Vector2.Distance(transform.position, EndPoint.transform.position) >= MinFallDistance)
+        
+        if(Vector2.Distance(transform.position, StartPoint.transform.position) <= 0f)
         {
             GoBack = false;
             GoForward = true;
         }
 
+        if (Vector2.Distance(transform.position, EndPoint.transform.position) <= 0f)
+        {
+            _waitTime += Time.deltaTime;
+
+            if (_waitTime >= MaxWaitTime)
+            {
+                GoBack = true;
+                GoForward = false;
+                _waitTime = 0f;
+            }
+        }
+        
 
         if (GoBack)
         {
             GoForward = false;
-            transform.position = Vector2.MoveTowards(transform.position, StartPoint.transform.position, MovementSpeed * Time.deltaTime);
-            Debug.Log(Vector2.Distance(transform.position, StartPoint.transform.position));
-            Debug.Log(Vector2.Distance(transform.position, EndPoint.transform.position));
-            Debug.Log("move here");
+            transform.position = Vector2.MoveTowards(transform.position, StartPoint.transform.position, RetractSpeed * Time.deltaTime);
         }
         else
         {
             if(GoForward)
             {
                 GoBack = false;
-                transform.position = Vector2.MoveTowards(transform.position, EndPoint.transform.position, MovementSpeed * Time.deltaTime);
-                
-                Debug.Log("move forward");
+                transform.position = Vector2.MoveTowards(transform.position, EndPoint.transform.position, FallingSpeed * Time.deltaTime);
             }
         }    
 
