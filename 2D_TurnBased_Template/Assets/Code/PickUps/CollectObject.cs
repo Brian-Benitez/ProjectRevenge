364fa8dd68cbba0d1a5a0fe0d98d8caad269476e
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2025-12-21 20:53:48 -0500

    Created Prefabs + made pickups more interactable
    I didnt want the player to collect it without realizing it, so I made it so they have to click E to grab objects

--- 2D_TurnBased_Template/Assets/Code/PickUps/CollectObject.cs
+++ 2D_TurnBased_Template/Assets/Code/PickUps/CollectObject.cs
@@ -2,13 +2,31 @@ using UnityEngine;
 
 public class CollectObject : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    public GameObject EKeyButtonGO;
+
+    bool WithinRange = false;   
+
+    private void Update()
     {
-        if(collision.CompareTag("Player"))
+        if(Input.GetKeyDown(KeyCode.E) && WithinRange)
         {
             PlayerInventory.Instance.AddObjectToInventory(this.gameObject);
             Debug.Log("pick up object");
             gameObject.SetActive(false);//do something with this later
         }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            EKeyButtonGO.SetActive(true);
+            WithinRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        EKeyButtonGO.SetActive(false);
+        WithinRange = false;
     }
 }
