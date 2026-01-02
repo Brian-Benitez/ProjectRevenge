1f2d1940b4691aa65716cb43e7d0e32c2b77354b
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2026-01-02 11:50:30 -0500

    Adjust: Player can pick up health instead of walking on it to heal

--- 2D_TurnBased_Template/Assets/Code/PickUps/HealthPickUp.cs
+++ 2D_TurnBased_Template/Assets/Code/PickUps/HealthPickUp.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class HealthPickUp : BasePickUp
 {
+    public GameObject EKeyButtonGO;
+    public bool WithinRange = false;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && WithinRange)
+        {
+            gameObject.SetActive(false);
+            HealPlayer();
+            PlayerController.Instance.Player.GetComponent<BaseCharacter>().UpdatePlayersStats();
+            Debug.Log("healed");
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            HealPlayer();
-            PlayerController.Instance.Player.GetComponent<BaseCharacter>().UpdatePlayersStats();
-            Debug.Log("healed");
-            Destroy(gameObject);
+            EKeyButtonGO.SetActive(true);
+            WithinRange = true;
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        EKeyButtonGO.SetActive(false);
+        WithinRange = false;
     }
 }
