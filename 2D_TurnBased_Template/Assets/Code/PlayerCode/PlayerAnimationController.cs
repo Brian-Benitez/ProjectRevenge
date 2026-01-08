6f0b631276b638902636952ba75d6e9335c51e7f
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2026-01-07 22:42:36 -0500

    Added: Melee attack
    I still need to rename stuff and adjust heavy. But its still in and thats helpful for me

--- 2D_TurnBased_Template/Assets/Code/PlayerCode/PlayerAnimationController.cs
+++ 2D_TurnBased_Template/Assets/Code/PlayerCode/PlayerAnimationController.cs
@@ -4,6 +4,22 @@ public class PlayerAnimationController : MonoBehaviour
 {
     public Animator PlayerAnimator;
 
+    public PlayerMeleeAttack PlayerMeleeAttackRef;
+
+
+    private void Update()
+    {
+        if (PlayerMeleeAttackRef.IsAttacking)
+            IsAttacking();
+        else
+            IsNotAttacking();
+    }
+
+    //Walking animations bools
     public void IsMoving() => PlayerAnimator.SetBool("IsMoving", true);
     public void IsNotMoving() => PlayerAnimator.SetBool("IsMoving", false);
+
+    //Attacking animations bools
+    public void IsAttacking() => PlayerAnimator.SetBool("IsAttacking", true);
+    public void IsNotAttacking() => PlayerAnimator.SetBool("IsAttacking", false);
 }
