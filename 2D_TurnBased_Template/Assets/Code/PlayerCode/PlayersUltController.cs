a91916e26d843a376c8645fd287c196edcc67bf4
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2026-01-10 22:50:08 -0500

    Work in progress: Changing players stats and more

--- 2D_TurnBased_Template/Assets/Code/PlayerCode/PlayersUltController.cs
+++ 2D_TurnBased_Template/Assets/Code/PlayerCode/PlayersUltController.cs
@@ -8,7 +8,18 @@ public class PlayersUltController : MonoBehaviour
     public int UltPoints;
     public int MaxUltPoints;
     public float UltDuration;
+    public float MaxUltDuration;
     public KeyCode UltActivationKey;
+
+    public int BoostedMovementSpeed;
+    public float LoweredDashCoolDown;
+
+    public int MeleeUpgradeDam;
+    public int RangeUpgradeDam;
+
+    public PlayerMovement PlayerMovementRef;
+    public PlayerMeleeAttack PlayerMeleeAttackRef;
+    public PlayerInfo PlayerInfoRef;
 
     private void Awake()
     {
@@ -21,12 +32,52 @@ public class PlayersUltController : MonoBehaviour
     {
         if(Input.GetKeyDown(UltActivationKey))
         {
-            if (!IsUlted && UltPoints == MaxUltPoints)
+            if(!IsUlted && UltPoints == MaxUltPoints)
             {
-                IsUlted = true;
+                if (UltDuration <= 0)
+                {
+                    IsUlted = false;
+                    SetPlayerToNormalStats();
+                    RemoveAllUltPoints();
+                    UltDuration += MaxUltDuration;
+                }
+                else
+                {
+                    UltDuration -= Time.deltaTime;
+                    IsUlted = true;
+                    ActivateUlt();
+                }
             }
+            
         }
-      
+    }
+
+    public void ActivateUlt()
+    {
+        //Movement upgrade
+        PlayerMovementRef.PlayerSpeed += BoostedMovementSpeed;
+        PlayerMovementRef.DashCoolDown -= LoweredDashCoolDown;
+
+        //Melee upgrade
+        PlayerMeleeAttackRef.PlayerLightAttkDamg += MeleeUpgradeDam;
+        PlayerMeleeAttackRef.PlayerHeavyAttkDamg += MeleeUpgradeDam;
+
+        //Range upgrade
+        PlayerInfoRef.RangeDamg += RangeUpgradeDam;
+    }
+
+    public void SetPlayerToNormalStats()
+    {
+        //Movement upgrade
+        PlayerMovementRef.PlayerSpeed -= BoostedMovementSpeed;
+        PlayerMovementRef.DashCoolDown += LoweredDashCoolDown;
+
+        //Melee upgrade
+        PlayerMeleeAttackRef.PlayerLightAttkDamg -= MeleeUpgradeDam;
+        PlayerMeleeAttackRef.PlayerHeavyAttkDamg -= MeleeUpgradeDam;
+
+        //Range upgrade
+        PlayerInfoRef.RangeDamg -= RangeUpgradeDam;
     }
     /// <summary>
     /// Made it like this so i can add objs that will max it or just give one. Only place to add it.
