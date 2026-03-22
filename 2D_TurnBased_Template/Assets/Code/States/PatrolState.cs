05f5bf4f740455c92df58794fc0067c52c290e0b
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2026-03-22 12:54:12 -0400

    Created: Patrol state +  transition towards movement state
    Still bugs like when you get hit by enemy, he just stands there and does not chase player. Fixing that next. Also all enemies need a point to patrol in

---
+++ 2D_TurnBased_Template/Assets/Code/States/PatrolState.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class PatrolState : State
+{
+    public GameObject PatrolSpotOne;
+    public GameObject PatrolSpotTwo;
+    public GameObject EnemyGO;
+
+    public bool DidMakeItToFirstPoint = false;
+
+    public BaseEnemy BaseEnemyRef;
+    public EnemyAggroDistance EnemyAggroDistanceRef;
+    public MovementState MovementStateRef;
+
+    private void Update()
+    {
+        if(EnemyAggroDistanceRef.IsAggro == false)
+        {
+            Patrol();
+        }
+    }
+
+    private void Patrol()
+    {
+        if (Vector2.Distance(EnemyGO.transform.position, PatrolSpotOne.transform.position) < 0.5f)
+            DidMakeItToFirstPoint = true;
+
+        if (Vector2.Distance(EnemyGO.transform.position, PatrolSpotTwo.transform.position) < 0.5f)
+            DidMakeItToFirstPoint = false;
+
+        if (Vector2.Distance(EnemyGO.transform.position, PatrolSpotOne.transform.position) > 0.5f && !DidMakeItToFirstPoint)
+        {
+            EnemyGO.transform.position = Vector2.MoveTowards(EnemyGO.transform.position, PatrolSpotOne.transform.position, BaseEnemyRef.EnemySpeed * Time.deltaTime);
+            Debug.Log("moving towards next spot");
+        }
+        
+
+        
+        if(Vector2.Distance(transform.position, PatrolSpotTwo.transform.position) > 0.5f && DidMakeItToFirstPoint)
+        {
+            EnemyGO.transform.position = Vector2.MoveTowards(EnemyGO.transform.position, PatrolSpotTwo.transform.position, BaseEnemyRef.EnemySpeed * Time.deltaTime);
+            Debug.Log("going towards orginal spot");
+        }
+       
+    }
+
+    public override State RunCurrentState()
+    {
+        if (EnemyAggroDistanceRef.IsAggro)
+            return MovementStateRef;
+        else
+            return this;
+    }
+}
