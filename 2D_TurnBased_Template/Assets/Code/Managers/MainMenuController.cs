91696ee5b8603e0efbfd93121c0ebb0f6f596fc9
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2026-05-06 20:15:20 -0400

    Created: Starting menu + added functionality to quit and start game buttons
    They work, just wanted to add all functionality before art.

---
+++ 2D_TurnBased_Template/Assets/Code/Managers/MainMenuController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject StartMenuPrefab;
+    public GameObject OptionsMenuPrefab;
+
+    private void Start()
+    {
+        StartMenuPrefab.SetActive(true);
+    }
+    public void StartingGame()
+    {
+        //add fade to black...
+        StartMenuPrefab.SetActive(false);
+        //place player in tutorial area...
+    }
+
+    public void OptionMenuOpen()
+    {
+        //add small pop up on menu...
+        Debug.Log("does not have menu made yet...");
+        //OptionsMenuPrefab.SetActive(true);
+    }
+    public void QuitGame() => Application.Quit();
+}
